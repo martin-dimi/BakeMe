0d0c129f09726f2f0b2d4931eebb12709936199e
TheWalkingFridge <martin.dimi97@gmail.com>
2017-10-26 22:03:48 +0100

    Done with fetching recipes

---
+++ app/src/main/java/com/niquid/personal/bakeme/models/Recipe.java
@@ -0,0 +1,23 @@
+package com.niquid.personal.bakeme.models;
+
+import java.util.List;
+
+
+public class Recipe {
+
+    private int id;
+    private String name;
+    private List<Ingredient> ingredients;
+    private List<Step> steps;
+    private int servings;
+    private String image;
+
+    public Recipe(int id, String name, List<Ingredient> ingredients, List<Step> steps, int servings, String image) {
+        this.id = id;
+        this.name = name;
+        this.ingredients = ingredients;
+        this.steps = steps;
+        this.servings = servings;
+        this.image = image;
+    }
+}
---
+++ app/src/main/java/com/niquid/personal/bakeme/utils/NetworkUtils.java
@@ -0,0 +1,47 @@
+package com.niquid.personal.bakeme.utils;
+
+import org.apache.commons.io.IOUtils;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.nio.charset.StandardCharsets;
+
+import timber.log.Timber;
+
+public class NetworkUtils {
+
+    private static final String RECEPIES_PATH = "https://d17h27t6h515a5.cloudfront.net/topher/2017/May/59121517_baking/baking.json";
+    private static final URL RECEPIES_URL = getURL(RECEPIES_PATH);
+
+    private static URL getURL(String path){
+        URL url = null;
+
+        try {
+            url = new URL(path);
+        } catch (MalformedURLException e) {
+            Timber.e("Couldn't create url");
+        }
+
+        return url;
+    }
+
+    public static String getRecepiesJSON(){
+        String recepies = null;
+
+        try {
+            HttpURLConnection connection = (HttpURLConnection) RECEPIES_URL.openConnection();
+            InputStream input = connection.getInputStream();
+
+            recepies = IOUtils.toString(input, StandardCharsets.UTF_8);
+            input.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return recepies;
+    }
+
+}
